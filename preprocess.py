3746a9f48c6e74eee2b18f2319f8e5635ca2d8bb
Lin-John <47441303+Lin-John@users.noreply.github.com>
2019-06-17 23:34:26 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,11 @@
 import os
 import datetime
+import time
+import math
 import numpy as np
 from openpyxl import load_workbook
 from collections import defaultdict
+from bisect import bisect_left, bisect_right
 
 
 def xls2txt(idir, odir, start, end):
@@ -55,6 +58,7 @@ def xls2txt(idir, odir, start, end):
                         f.write('{}\n'.format(str(col[2 + i].value)))
             cur = tmp
         cur = timelist[-1].value + dtime
+
 
 def is_number(s):
     """Judges whether a string is a number
@@ -142,9 +146,9 @@ def feature_selection(idir, odir):
             f.write('{}\n'.format(feature))
 
 
-def gaussian_detection(features, length, phi=1.96):
+def gaussian_detection(features, phi=1.96):
     """Assume the residuals have a Gaussian distribution,
-       and regard the data that is too far away from mean as an abnormal point.
+       and regard the data that is too far away from mean as an abnormity.
 
     Args:
         features: a dict whose keys are the names of feature and the values are the corresponding records.
@@ -152,11 +156,12 @@ def gaussian_detection(features, length, phi=1.96):
         phi: the quantile of the Gaussion distribution.
 
     Returns:
-        a list of intergers, the number means how many features raise abnormity at this moment.
+        a dict whose keys are the names of feature and the values are the corresponding abnormities.
     """
-    count = np.zeros(length)
+    abnormities = defaultdict(list)
     for feature in features:
         data = features[feature]
+        length = len(data) - 1
         res = data[1:] - data[:-1]
         std = np.std(res)
         mean = np.mean(res)
@@ -164,8 +169,133 @@ def gaussian_detection(features, length, phi=1.96):
         ub = mean + phi * std
         for i in range(length):
             if res[i] < lb or res[i] > ub:
-                count[i] += 1
+                abnormities[feature].append(i)
+    return abnormities
+
+
+def str2timestamp(datetimes):
+    """Transports a list of strings to a list of timestamp
+    """
+
+    def s2t(datetime):
+        return time.mktime(time.strptime(datetime, '%Y-%m-%d %H:%M:%S'))
+
+    return list(map(s2t, datetimes))
+
+
+def nearest_r_abnormity(g_abnormities, r_abnormities):
+    """Calculates the time delay between the abnormities found by the algorithm
+       and the nearest recorded abnormity after it.
+
+    Args:
+        g_abnormities: the abnormities found by the algorithm.
+        r_abnormities: the recorded abnormities.
+
+    Returns:
+        a list of the time delay between the abnormities found by the algorithm
+        and the nearest recorded abnormity after it.
+    """
+    res = []
+    for ts in g_abnormities:
+        if ts > r_abnormities[-1]:
+            res.append(float("inf"))
+        else:
+            idx = bisect_left(r_abnormities, ts)
+            res.append(r_abnormities[idx] - ts)
+    return res
+
+
+def nearest_g_abnormity(g_abnormities, r_abnormities):
+    """Calculates the time delay between the recorded abnormities
+       and the nearest the abnormity found by the algorithm before it.
+
+    Args:
+        g_abnormities: the abnormities found by the algorithm.
+        r_abnormities: the recorded abnormities.
+
+    Returns:
+        a list of the time delay between the recorded abnormities
+        and the nearest the abnormity found by the algorithm before it.
+    """
+    res = []
+    for ts in r_abnormities:
+        if ts < g_abnormities[0]:
+            res.append(float("inf"))
+        else:
+            idx = bisect_right(g_abnormities, ts)
+            res.append(ts - g_abnormities[idx - 1])
+    return res
+
+
+def adj_precision(g_abnormities, r_abnormities, weight=lambda x: math.exp(-x / 86400)):
+    """Calculate the adjusted precision of the result
+       which is weighted by the time delay.
+
+    Args:
+        g_abnormities: the abnormities found by the algorithm.
+        r_abnormities: the recorded abnormities.
+
+    Returns:
+        the adjusted precision
+    """
+    delay = nearest_g_abnormity(g_abnormities, r_abnormities)
+    return sum(map(weight, delay)) / len(delay)
+
+
+def adj_recall(g_abnormities, r_abnormities, weight=lambda x: math.exp(-x / 86400)):
+    """Calculate the adjusted recall of the result
+       which is weighted by the time delay.
+
+    Args:
+        g_abnormities: the abnormities found by the algorithm.
+        r_abnormities: the recorded abnormities.
+
+    Returns:
+        the adjusted recall
+    """
+    delay = nearest_r_abnormity(g_abnormities, r_abnormities)
+    return sum(map(weight, delay)) / len(delay)
+
+
+def adj_f1(g_abnormities, r_abnormities):
+    """"Calculate the adjusted F1_score of the result
+
+    Args:
+        g_abnormities: a list of datetimes when the algorithm raises an abnormity.
+        r_abnormities: a list of datetimes when the there is a recorded abnormity.
+
+    Returns:
+        the adjusted F1_score.
+    """
+    ap = adj_precision(g_abnormities, r_abnormities)
+    ar = adj_recall(g_abnormities, r_abnormities)
+    return 2 / (1 / ap + 1 / ar)
+
+
+def weighted_count(abnormities, adj_f1_scores, length):
+    """Weights the features by its adjusted F1_score and the
+       number of abnormities it raises.
+
+    Args:
+        abnormities: a dict whose keys are the names of feature
+                     and the values are the abnormities it raises.
+        adj_F1_scores: a dict whose keys are the names of feature
+                       and the values are corresponding adjusted F1_score.
+        length: the number of residuals.
+
+    Returns:
+        a list of floats, the number means how many features raise abnormity(weighted) at this moment.
+    """
+    count = np.zeros(length)
+    for feature in abnormities:
+        abnormity = abnormities[feature]
+        if abnormity:
+            weight = adj_f1_scores[feature] / np.log(
+                1 + len(abnormity))  # The more abnormities a feature raise, the less important it is
+            for dtime in abnormity:
+                count[dtime] += weight
     return count
+
 
 def get_abnormity(count, date, N):
     """Returns the dates in which most abnormities are raised.
@@ -180,6 +310,7 @@ def get_abnormity(count, date, N):
         dd[date[i].split(' ')[0]] += 1
     return sorted(sorted([key for key in dd], key=lambda x: -dd[x]))
 
+
 def main(idir, odir, featuredir, N=50, generatetxt=False, selectfeatures=False):
     if generatetxt:
         start = [2017, 1, 1]
@@ -188,13 +319,24 @@ def main(idir, odir, featuredir, N=50, generatetxt=False, selectfeatures=False):
     if selectfeatures:
         feature_selection(odir, featuredir)
     date = readdate(os.path.join(odir, 'datetime.txt'))
-    length = len(date)-1
+    length = len(date) - 1
     features = {}
     with open(os.path.join(featuredir, 'features.txt'), 'r') as f:
         for line in f:
-            features[line] = readdata(os.path.join(odir, line.strip()))
-    count = gaussian_detection(features, length, phi=1.96)
+            features[line.strip()] = readdata(os.path.join(odir, line.strip()))
+    r_abnormities = []
+    with open(os.path.join(featuredir, 'abnormities.txt'), 'r') as f:
+        for line in f:
+            r_abnormities.append(line.strip())
+    r_abnormities_ts = str2timestamp(r_abnormities)
+    abnormities = gaussian_detection(features, phi=1.96)
+    f1_scores = {}
+    for feature in abnormities:
+        abnormity = abnormities[feature]
+        f1_scores[feature] = adj_f1(str2timestamp(date[i] for i in abnormity), r_abnormities_ts)
+    count = weighted_count(abnormities, F1_scores, length)
     print(get_abnormity(count, date, N))
+
 
 if __name__ == '__main__':
     print("begin")
